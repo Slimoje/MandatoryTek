b491aecbf7f0cb5db847eb09a71a13ebdf605ba3
Slimoje <mart187c@stud.kea.dk>
2021-11-24 10:57:33 +0100

    sdad

---
+++ src/main/java/martin/kea/mandatorytek/MandatoryTekApplication.java
@@ -0,0 +1,13 @@
+package martin.kea.mandatorytek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatoryTekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandatoryTekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/martin/kea/mandatorytek/MandatoryTekApplicationTests.java
@@ -0,0 +1,13 @@
+package martin.kea.mandatorytek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatoryTekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
