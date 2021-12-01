505b90a03107f3eeaba77262a61d48d3e413b99d
Mads Kolbye <christiane1662@yahoo.dk>
2021-12-01 10:12:18 +0100

    rapo

--- src/main/java/martin/kea/mandatorytek/models/User.java
+++ src/main/java/martin/kea/mandatorytek/models/User.java
@@ -1,11 +1,27 @@
 package martin.kea.mandatorytek.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Data
+@Table(name="users")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+
+    @Column
     private String email;
+
+    @Column
     private String host;
+
+    @Column
     private boolean internal;
 
     private List<User> friends;
