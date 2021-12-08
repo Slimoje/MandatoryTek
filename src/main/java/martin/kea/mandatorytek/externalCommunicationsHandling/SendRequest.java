1511dfc637119138c554083aaa2399a3c7c83f85
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-12-08 09:22:32 +0100

    Added Jon's code to the SendRequest-class

--- src/main/java/martin/kea/mandatorytek/externalCommunicationsHandling/SendRequest.java
+++ src/main/java/martin/kea/mandatorytek/externalCommunicationsHandling/SendRequest.java
@@ -1,4 +1,33 @@
 package martin.kea.mandatorytek.externalCommunicationsHandling;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Controller
 public class SendRequest {
+    final String API_GREETING_POST = "http://localhost:9091/friendship";
+
+    @PostMapping("/sendPostGreeting")
+    public String sendGreeting(Model model){
+        WebClient webClient = WebClient.builder()
+                .baseUrl(API_GREETING_POST)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        String request = "\"hi from client\"";
+        String key= "\"request\"";
+        String response = webClient.post()
+                .body(Mono.just("{" + key + ":" + request + "}"), String.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        model.addAttribute("greeting", response);
+        return "index";
+    }
 }
