84aab65474ace7341926d72c97334d19490a0bd4
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-12-08 09:27:45 +0100

    Added Jon's method to receive a request and send back a response to the ReceiveRequest-class.
    By the way, I added a new dependency to make it all work.

--- src/main/java/martin/kea/mandatorytek/externalCommunicationsHandling/ReceiveRequest.java
+++ src/main/java/martin/kea/mandatorytek/externalCommunicationsHandling/ReceiveRequest.java
@@ -1,4 +1,17 @@
 package martin.kea.mandatorytek.externalCommunicationsHandling;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReceiveRequest {
+
+    @RequestMapping(method = RequestMethod.POST, path = "/friendship")
+    public ResponseEntity<String> postGreetingRoot(@RequestBody Object req){
+        System.out.println("Root request: " + req);
+        return ResponseEntity.ok("Hello from Server 2 (using POST)");
+    }
 }
